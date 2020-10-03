989f8c3bb3d6e55e9c37f2a0f5d4175118be0737
“Anthony <tonywk@ya.ru>
2020-10-03 20:14:51 +0400

    hello world app

---
+++ first_one/src/LearnMain.java
@@ -0,0 +1,7 @@
+public class LearnMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Java world!");
+        System.out.println(42);
+        System.out.println("\n\tjava" + 11);
+    }
+}
